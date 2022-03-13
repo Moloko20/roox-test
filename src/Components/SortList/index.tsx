5d6e1f67d1947820fe0fdee8df3df3747caf2449
Moloko20 <alexvfrontend@gmail.com>
2022-03-13 22:12:43 +0300

    feat(sort list): add optimization and hendler for buttons

--- src/Components/SortList/index.tsx
+++ src/Components/SortList/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
+
 import { Button } from '../UI/Button'
+
+import { Context } from 'Contexts/context'
 
 const buttonsTitles = ['по городу', 'по компании']
 
-export function SortList() {
+export function SortListComponent() {
+    const sorting = React.useContext(Context)
+
+    const btnHendlerProp = React.useCallback((title: string) => {
+        sorting(title)
+    }, [])
+
     require('./index.scss')
+
+    console.log('rerender sortlist')
 
     return (
         <aside className="sortlist">
             <h2>Сортировка</h2>
-            <Button title={buttonsTitles[0]} />
-            <Button title={buttonsTitles[1]} />
+            <Button title={buttonsTitles[0]} btnClick={btnHendlerProp} />
+            <Button title={buttonsTitles[1]} btnClick={btnHendlerProp} />
         </aside>
     )
 }
+
+export const SortList = React.memo(SortListComponent)
