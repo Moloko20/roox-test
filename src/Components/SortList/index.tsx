f759e09fe30c8f5e3e165f31944985ebdfa99c3e
Moloko20 <alexvfrontend@gmail.com>
2022-03-13 18:47:49 +0300

    feat(sort list): add data for users and loader logic

--- src/Components/SortList/index.tsx
+++ src/Components/SortList/index.tsx
@@ -1,24 +1,6 @@
 import React from 'react'
 import { Button } from '../UI/Button'
 
-// export type FaceTypes = 'lose' | 'win' | 'game' | 'start'
-
-// type stateList = {
-//     [key in FaceTypes]: string
-// }
-
-// const STATE_LIST: stateList = {
-//     lose: '💀',
-//     win: '😎',
-//     game: '🙂',
-//     start: '🙂',
-// }
-
-// type faceProps = {
-//     state: FaceTypes
-//     onResetGame: () => void
-// }
-
 const buttonsTitles = ['по городу', 'по компании']
 
 export function SortList() {
