52d7cd953b79979031d1f32142d35fa9478d532f
Moloko20 <alexvfrontend@gmail.com>
2022-03-13 22:09:15 +0300

    perf(app): replace sorting logic to app, add context

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,16 +2,91 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { SortList } from 'Components/SortList'
-import { UsersList } from 'Components/UserList'
+import { UsersList, UserType } from 'Components/UserList'
+import { Loader } from 'Components/UI/Loader'
+
+import { getUsers } from 'Services/getUsers'
+
+import { Context } from 'Contexts/context'
+
+const sortByCity = (a: UserType, b: UserType) => {
+    const cityA = a.address.city.toLowerCase()
+    const cityB = b.address.city.toLowerCase()
+
+    if (cityA > cityB) {
+        return 1
+    } else if (cityA < cityB) {
+        return -1
+    }
+    return 0
+}
+
+const sortByCompany = (a: UserType, b: UserType) => {
+    const companyA = a.company.name.toLowerCase()
+    const companyB = b.company.name.toLowerCase()
+
+    if (companyA > companyB) {
+        return 1
+    } else if (companyA < companyB) {
+        return -1
+    }
+    return 0
+}
 
 function App() {
+    const [isLoading, setIsLoading] = React.useState(false)
+    const [users, setUsers] = React.useState([])
+
+    React.useEffect(() => {
+        const loadData = async () => {
+            setIsLoading(true)
+
+            await getUsers()
+                .then(data => setUsers(data))
+                .catch(error => console.error(error))
+
+            setIsLoading(false)
+        }
+
+        loadData()
+    }, [])
+
+    const sorting = (title: string) => {
+        switch (title) {
+            case 'по городу':
+                setUsers(prevState => {
+                    const nextState = [].concat(prevState).sort(sortByCity)
+
+                    return nextState
+                })
+                break
+
+            case 'по компании':
+                setUsers(prevState => {
+                    const nextState = [].concat(prevState).sort(sortByCompany)
+
+                    return nextState
+                })
+                break
+        }
+    }
+
     require('./index.scss')
 
     return (
-        <section className="users">
-            <SortList />
-            <UsersList />
-        </section>
+        <Context.Provider value={sorting}>
+            <section className="users">
+                <SortList />
+
+                {isLoading ? (
+                    <div className="loader-wrapper">
+                        <Loader />
+                    </div>
+                ) : (
+                    <UsersList users={users} />
+                )}
+            </section>
+        </Context.Provider>
     )
 }
 
