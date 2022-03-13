0e07dbd4884bdffd3ccb3589e7ab22c373ddc608
Moloko20 <alexvfrontend@gmail.com>
2022-03-13 22:10:56 +0300

    feat(user list): add optimization

--- src/Components/UserList/index.tsx
+++ src/Components/UserList/index.tsx
@@ -1,53 +1,42 @@
 import React from 'react'
 
 import { UserItem } from 'Components/UserItem'
-import { Loader } from 'Components/UI/Loader'
 
-import { getUsers } from 'Services/getUsers'
+export type UserType = {
+    id: number
+    name: string
+    address: {
+        city: string
+    }
+    company: {
+        name: string
+    }
+}
 
-export function UsersList() {
-    const [users, setUsers] = React.useState([])
-    const [isLoading, setIsLoading] = React.useState(false)
+type UsersType = {
+    users: UserType[]
+}
 
-    React.useEffect(() => {
-        const loadData = async () => {
-            setIsLoading(true)
-
-            await getUsers()
-                .then(data => setUsers(data))
-                .catch(error => console.error(error))
-
-            setIsLoading(false)
-        }
-
-        loadData()
-    }, [])
-
+export function UsersList({ users }: UsersType) {
     require('./index.scss')
 
     return (
         <div className="userlist">
             <h1>Список пользователей</h1>
-            {isLoading ? (
-                <div className="loader-wrapper">
-                    <Loader />
-                </div>
-            ) : (
-                users.map(user => {
-                    if (users.length > 0) {
-                        return (
-                            <UserItem
-                                key={user.id}
-                                name={user.name}
-                                city={user.address.city}
-                                company={user.company.name}
-                            />
-                        )
-                    }
+            {users.map(user => {
+                if (users.length > 0) {
+                    return (
+                        <UserItem
+                            key={user.id}
+                            name={user.name}
+                            city={user.address.city}
+                            company={user.company.name}
+                        />
+                    )
+                }
 
-                    return <span>Пользователи не найдены</span>
-                })
-            )}
+                return <span>Пользователи не найдены</span>
+            })}
             <span className="userlist__quantity">Найдено {users.length} пользователей</span>
         </div>
     )
