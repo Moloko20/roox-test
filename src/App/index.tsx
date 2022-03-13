77116e603a2384b5803544d5c39f7600d1bee50c
Moloko20 <alexvfrontend@gmail.com>
2022-03-13 18:38:06 +0300

    refactor(app): delete comments

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,47 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { SortList } from 'Components/SortList'
-import { UserList } from 'Components/UserList'
-
-// const DIFFICULTIES: Select = [
-//     { value: "easy", title: "Easy" },
-//     { value: "normal", title: "Normal" },
-//     { value: "hard", title: "Hard" },
-// ]
+import { UsersList } from 'Components/UserList'
 
 function App() {
-    // const [diff, setDiff] = React.useState<string>('easy')
-    // const [gameState, setGameState] = React.useState<FaceTypes>('start')
-    // const gameResetFn = React.useRef<() => void>()
-
-    // const changeDifficulty = React.useCallback((value: string) => {
-    //     setDiff(value)
-    // }, [])
-
-    // const changeEmoji = React.useCallback((value: FaceTypes) => {
-    //     setGameState(value)
-    // }, [])
-
-    // const gameResetter = React.useCallback(resetter => {
-    //     gameResetFn.current = resetter
-    // }, [])
-
-    // const resetHandler = () => {
-    //     gameResetFn.current()
-    // }
-
     require('./index.scss')
 
     return (
         <section className="users">
             <SortList />
-            <UserList />
-            {/* <div className="menu">
-                <Select options={DIFFICULTIES} value={diff} onChange={changeDifficulty} />
-                <Face state={gameState} onResetGame={resetHandler} />
-                <Timer state={gameState} />
-            </div>
-            <Board difficulty={diff} onGameState={changeEmoji} getReset={gameResetter} /> */}
+            <UsersList />
         </section>
     )
 }
