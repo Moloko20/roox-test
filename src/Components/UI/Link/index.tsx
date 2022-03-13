c75380f0c724e43727e396d48f7b98149cb66d8f
Moloko20 <alexvfrontend@gmail.com>
2022-03-13 22:15:57 +0300

    refactor(link): delete comments

--- src/Components/UI/Link/index.tsx
+++ src/Components/UI/Link/index.tsx
@@ -1,23 +1,5 @@
 import React from 'react'
 
-// export type FaceTypes = 'lose' | 'win' | 'game' | 'start'
-
-// type stateList = {
-//     [key in FaceTypes]: string
-// }
-
-// const STATE_LIST: stateList = {
-//     lose: '💀',
-//     win: '😎',
-//     game: '🙂',
-//     start: '🙂',
-// }
-
-// type faceProps = {
-//     state: FaceTypes
-//     onResetGame: () => void
-// }
-
 export function Link() {
     return (
         <a className="useritem__link" href="#">
