5cdcaa58dad12961dafaac8ace3749421eb245f7
Moloko20 <alexvfrontend@gmail.com>
2022-03-15 14:44:10 +0300

    feat(buunot): add class name logic

--- src/Components/UI/Button/index.tsx
+++ src/Components/UI/Button/index.tsx
@@ -3,17 +3,20 @@ import React from 'react'
 type ButtonProps = {
     title: string
     btnClick: (title: string) => void
+    btnClass?: string
 }
 
-export function ButtonComponent({ title, btnClick }: ButtonProps) {
+export function ButtonComponent({ title, btnClick, btnClass = '' }: ButtonProps) {
     const btnHendler = () => {
         btnClick(title)
     }
 
+    const nameClass = `button ${btnClass}`
+
     require('./index.scss')
 
     return (
-        <button className="button" onClick={btnHendler}>
+        <button className={nameClass} onClick={btnHendler}>
             {title}
         </button>
     )
