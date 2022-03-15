6030d355cdc4365ff9c07c9184a81d41d2b79ebb
Moloko20 <alexvfrontend@gmail.com>
2022-03-15 18:59:48 +0300

    feat(profile): add autocomplete

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -2,51 +2,67 @@ import React from 'react'
 
 import { Button } from 'Components/UI/Button'
 import { Input } from 'Components/UI/Input'
+import { UserType } from 'Components/UserItem'
 
-const inputs = [
-    {
-        title: 'Name',
-        type: 'text',
-    },
-    {
-        title: 'User name',
-        type: 'text',
-    },
-    {
-        title: 'E-mail',
-        type: 'text',
-    },
-    {
-        title: 'Street',
-        type: 'text',
-    },
-    {
-        title: 'City',
-        type: 'text',
-    },
-    {
-        title: 'Zip code',
-        type: 'text',
-    },
-    {
-        title: 'Phone',
-        type: 'text',
-    },
-    {
-        title: 'Website',
-        type: 'text',
-    },
-    {
-        title: 'Comment',
-        type: 'textarea',
-    },
-]
+type ProfileProps = {
+    user: UserType
+}
 
-export function Profile() {
-    require('./index.scss')
-    require('../UsersList/index.scss')
+export function Profile({ user }: ProfileProps) {
+    const inputs = [
+        {
+            title: 'Name',
+            type: 'text',
+            value: user?.name,
+        },
+        {
+            title: 'User name',
+            type: 'text',
+            value: user?.username,
+        },
+        {
+            title: 'E-mail',
+            type: 'text',
+            value: user?.email,
+        },
+        {
+            title: 'Street',
+            type: 'text',
+            value: user?.address.street,
+        },
+        {
+            title: 'City',
+            type: 'text',
+            value: user?.address.city,
+        },
+        {
+            title: 'Zip code',
+            type: 'text',
+            value: user?.address.zipcode,
+        },
+        {
+            title: 'Phone',
+            type: 'text',
+            value: user?.phone,
+        },
+        {
+            title: 'Website',
+            type: 'text',
+            value: user?.website,
+        },
+        {
+            title: 'Comment',
+            type: 'textarea',
+            value: '',
+        },
+    ]
 
     const editHandler = () => {}
+
+    const sendHandler = () => {}
+
+    require('./index.scss')
+    require('../UsersList/index.scss')
 
     return (
         <div className="userslist">
@@ -60,12 +76,17 @@ export function Profile() {
                     />
                 </div>
                 <div className="profile-body">
-                    {inputs.map(input => (
-                        <Input inputTitle={input.title} inputType={input.type} />
+                    {inputs.map((input, index) => (
+                        <Input
+                            key={index}
+                            inputTitle={input.title}
+                            inputType={input.type}
+                            inputValue={input.value}
+                        />
                     ))}
                 </div>
                 <div className="profile-footer">
-                    <Button title={'Отправить'} btnClick={editHandler} btnClass={'button--edit'} />
+                    <Button title={'Отправить'} btnClick={sendHandler} btnClass={'button--send'} />
                 </div>
             </div>
         </div>
