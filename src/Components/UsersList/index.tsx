516e1996e8c4660f58c94797d2669e5ce35a6dd0
Moloko20 <alexvfrontend@gmail.com>
2022-03-15 19:02:32 +0300

    perf(users list): change typing

--- src/Components/UsersList/index.tsx
+++ src/Components/UsersList/index.tsx
@@ -1,17 +1,6 @@
 import React from 'react'
 
-import { UserItem } from 'Components/UserItem'
-
-export type UserType = {
-    id: number
-    name: string
-    address: {
-        city: string
-    }
-    company: {
-        name: string
-    }
-}
+import { UserItem, UserType } from 'Components/UserItem'
 
 type UsersType = {
     users: UserType[]
@@ -26,19 +15,12 @@ export function UsersList({ users }: UsersType) {
                 <h1>Список пользователей</h1>
                 {users.map(user => {
                     if (users.length > 0) {
-                        return (
-                            <UserItem
-                                key={user.id}
-                                name={user.name}
-                                city={user.address.city}
-                                company={user.company.name}
-                            />
-                        )
+                        return <UserItem key={user.id} user={user} />
                     }
 
                     return <span>Пользователи не найдены</span>
                 })}
-                <span className="userlist__quantity">Найдено {users.length} пользователей</span>
+                <span className="userslist__quantity">Найдено {users.length} пользователей</span>
             </div>
         </>
     )
