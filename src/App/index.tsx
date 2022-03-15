4b45945c30c6d7e96d937e90bdb4a5c9e0b24820
Moloko20 <alexvfrontend@gmail.com>
2022-03-15 23:55:52 +0300

    feat(app): add new feats

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -11,35 +11,13 @@ import { Profile } from 'Components/Profile'
 import { getUsers } from 'Services/getUsers'
 
 import { Context } from 'Contexts/context'
-
-const sortByCity = (a: UserType, b: UserType) => {
-    const cityA = a.address.city.toLowerCase()
-    const cityB = b.address.city.toLowerCase()
-
-    if (cityA > cityB) {
-        return 1
-    } else if (cityA < cityB) {
-        return -1
-    }
-    return 0
-}
-
-const sortByCompany = (a: UserType, b: UserType) => {
-    const companyA = a.company.name.toLowerCase()
-    const companyB = b.company.name.toLowerCase()
-
-    if (companyA > companyB) {
-        return 1
-    } else if (companyA < companyB) {
-        return -1
-    }
-    return 0
-}
+import { formContext } from 'Contexts/formContext'
 
 function App() {
     const [isLoading, setIsLoading] = React.useState(false)
     const [users, setUsers] = React.useState([])
-    const [currentUser, setcurrentUser] = React.useState<UserType>()
+    const [inputs, setInputs] = React.useState([])
+    const [currentUser, setCurrentUser] = React.useState<UserType>()
 
     React.useEffect(() => {
         const loadData = async () => {
@@ -55,34 +33,71 @@ function App() {
         loadData()
     }, [])
 
-    const sorting = (title: string) => {
-        switch (title) {
-            case 'по городу':
-                setUsers(prevState => {
-                    const nextState = [].concat(prevState).sort(sortByCity)
+    const sortByCity = () => {
+        setUsers(prevState => {
+            const nextState = [].concat(prevState).sort(sorting)
 
-                    return nextState
-                })
-                break
+            return nextState
+        })
 
-            case 'по компании':
-                setUsers(prevState => {
-                    const nextState = [].concat(prevState).sort(sortByCompany)
+        const sorting = (a: UserType, b: UserType) => {
+            const cityA = a.address.city.toLowerCase()
+            const cityB = b.address.city.toLowerCase()
 
-                    return nextState
-                })
-                break
+            if (cityA > cityB) {
+                return 1
+            } else if (cityA < cityB) {
+                return -1
+            }
+            return 0
+        }
+    }
+
+    const sortByCompany = () => {
+        setUsers(prevState => {
+            const nextState = [].concat(prevState).sort(sorting)
+
+            return nextState
+        })
+
+        const sorting = (a: UserType, b: UserType) => {
+            const companyA = a.company.name.toLowerCase()
+            const companyB = b.company.name.toLowerCase()
+
+            if (companyA > companyB) {
+                return 1
+            } else if (companyA < companyB) {
+                return -1
+            }
+            return 0
         }
     }
 
     const setUser = (user: UserType) => {
-        setcurrentUser(user)
+        setCurrentUser(user)
+    }
+
+    const setInputsFN = (arrayOfInputs: []) => {
+        setInputs(arrayOfInputs)
+    }
+
+    const setInputValueFN = (value: string, title: string) => {
+        setInputs(prevState => {
+            return prevState.map(input => {
+                if (input.title === title) {
+                    input.value = value
+                }
+                return input
+            })
+        })
     }
 
     require('./index.scss')
 
     return (
-        <Context.Provider value={{ sortingFN: sorting, setUserFN: setUser }}>
+        <Context.Provider
+            value={{ sortByCity: sortByCity, sortByCompany: sortByCompany, setUserFN: setUser }}
+        >
             <section className="users">
                 <SortList />
 
@@ -93,7 +108,20 @@ function App() {
                 ) : (
                     <Routes>
                         <Route path="/" element={<UsersList users={users} />} />
-                        <Route path="/profile" element={<Profile user={currentUser} />} />
+                        <Route
+                            path="/profile"
+                            element={
+                                <formContext.Provider
+                                    value={{
+                                        inputs: inputs,
+                                        setInputs: setInputsFN,
+                                        setInputValue: setInputValueFN,
+                                    }}
+                                >
+                                    <Profile user={currentUser} />
+                                </formContext.Provider>
+                            }
+                        />
                     </Routes>
                 )}
             </section>
